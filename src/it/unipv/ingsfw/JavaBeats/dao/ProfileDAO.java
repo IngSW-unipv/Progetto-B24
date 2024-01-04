1838134f52f7cb4c203ef61cf1aa49501b9cae21
Giorgio Giacomotti <giorgio.giacomotti01@universitadipavia.it>
2024-01-04 17:12:43 +0100

    @Costruzione
    Correzione di ProfileDAO.

--- src/it/unipv/ingsfw/JavaBeats/dao/ProfileDAO.java
+++ src/it/unipv/ingsfw/JavaBeats/dao/ProfileDAO.java
@@ -4,7 +4,6 @@ import it.unipv.ingsfw.JavaBeats.controller.factory.DBManagerFactory;
 import it.unipv.ingsfw.JavaBeats.model.user.Artist;
 import it.unipv.ingsfw.JavaBeats.model.user.JBProfile;
 import it.unipv.ingsfw.JavaBeats.model.user.User;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -26,26 +25,41 @@ public class ProfileDAO implements IProfileDAO {
 
 
 
-    //METHODS:
+    //PUBLIC METHODS:
     @Override
     public void insert(JBProfile profile) {         //add new profile to database
         connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
-        PreparedStatement st;
+        PreparedStatement st1, st2, st3;
 
-        try {
-            String query =  "INSERT INTO Profile(username, mail, password, name, surname, biography, profilePicture)" +
+        try {       //insert JBProfile to Profile table
+            String q1 =  "INSERT INTO Profile(username, mail, password, name, surname, biography, profilePicture)" +
                             "VALUES(?, ?, ?, ?, ?, ?, ?);";
 
-            st = connection.prepareStatement(query);
-            st.setString(1, profile.getUsername());
-            st.setString(2, profile.getMail());
-            st.setString(3, profile.getPassword());
-            st.setString(4, profile.getName());
-            st.setString(5, profile.getSurname());
-            st.setString(6, profile.getBiography());
-            st.setBlob(7, profile.getProfilePicture());
+            st1 = connection.prepareStatement(q1);
+            st1.setString(1, profile.getUsername());
+            st1.setString(2, profile.getMail());
+            st1.setString(3, profile.getPassword());
+            st1.setString(4, profile.getName());
+            st1.setString(5, profile.getSurname());
+            st1.setString(6, profile.getBiography());
+            st1.setBlob(7, profile.getProfilePicture());
 
-            st.executeUpdate();
+            st1.executeUpdate();
+
+            if(profile instanceof User) {               //if JBProfile is a User insert it into User table
+                String q2=  "INSERT INTO User(mail, isVisible) VALUES(?, 1);";
+                st2 = connection.prepareStatement(q2);
+                st2.setString(1, profile.getMail());
+                st2.executeUpdate();
+
+            } else if (profile instanceof Artist) {     //if JBProfile is an Artist insert it to Artist table
+                String q3=  "INSERT INTO Artist(mail, totalListeners) VALUES(?, 0);";
+                st3= connection.prepareStatement(q3);
+                st3.setString(1, profile.getMail());
+                st3.executeUpdate();
+            } else {
+                //THROW EXCEPTION
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -53,7 +67,6 @@ public class ProfileDAO implements IProfileDAO {
 
         DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
     }
-
 
     @Override
     public void remove(JBProfile profile) {         //remove profile from database
@@ -75,7 +88,6 @@ public class ProfileDAO implements IProfileDAO {
         DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
     }
 
-
     @Override
     public void update(JBProfile profile) {         //push edited profile to database
         JBProfile oldProfile = get(profile);        //get profile as it is in DB to check for changes
@@ -84,70 +96,46 @@ public class ProfileDAO implements IProfileDAO {
 
         try {
 
-            if (profile.getUsername()!=oldProfile.getUsername() && profile.getUsername()!=null) {     //update username
+            if (profile.getUsername()!=null)        //check for null before using .equals to avoid exceptions
+                if(!(profile.getUsername().equals(oldProfile.getUsername())))
+                    updateUsername(profile);
 
-                String q1 = "UPDATE Profile SET username=? WHERE mail=?;";
+            if (profile.getPassword()!=null)
+                if(!(profile.getPassword().equals(oldProfile.getPassword())))
+                    updatePassword(profile);
 
-                PreparedStatement st1 = connection.prepareStatement(q1);
-                st1.setString(1, profile.getUsername());
-                st1.setString(2, profile.getMail());
+            if (profile.getName()!=null)
+                if(!(profile.getName().equals(oldProfile.getName())))
+                    updateName(profile);
 
-                st1.executeUpdate();
-            }
+            if (profile.getSurname()!=null)
+                if(!(profile.getSurname().equals(oldProfile.getSurname())))
+                    updateSurname(profile);
 
-            if(profile.getPassword()!=oldProfile.getPassword() && profile.getPassword()!=null) {     //update password
+            if (profile.getBiography()!=null)
+                if(!(profile.getBiography().equals(oldProfile.getBiography())))
+                    updateBiography(profile);
 
-                String q2 = "UPDATE Profile SET password=? WHERE mail=?;";
+            if (profile.getProfilePicture()!=null)
+                if(!(profile.getProfilePicture().equals(oldProfile.getProfilePicture())))
+                    updateProfilePicture(profile);
 
-                PreparedStatement st2 = connection.prepareStatement(q2);
-                st2.setString(1, profile.getPassword());
-                st2.setString(2, profile.getMail());
+            if((profile instanceof User) && oldProfile instanceof User) {       //check if profile is a USER
+                User userProfile = (User)profile;
+                User oldUserProfile = (User)oldProfile;
 
-                st2.executeUpdate();
-            }
+                if(userProfile.isVisible()!=oldUserProfile.isVisible())
+                    updateVisibility(userProfile);
 
-            if(profile.getName()!=oldProfile.getName() && profile.getName()!=null) {                //update name
+            } else if (profile instanceof Artist) {                             //check if profile is an ARTIST
+                Artist artistProfile = (Artist)profile;
+                Artist oldArtistProfile = (Artist)oldProfile;
 
-                String q3 = "UPDATE Profile SET name=? WHERE mail=?;";
+                if(artistProfile.getTotalListeners()!=oldArtistProfile.getTotalListeners())
+                    updateTotalListeners(artistProfile);
 
-                PreparedStatement st3 = connection.prepareStatement(q3);
-                st3.setString(1, profile.getName());
-                st3.setString(2, profile.getMail());
-
-                st3.executeUpdate();
-            }
-
-            if(profile.getSurname()!=oldProfile.getSurname() && profile.getSurname()!=null) {       //update surname
-
-                String q4 = "UPDATE Profile SET surname=? WHERE mail=?;";
-
-                PreparedStatement st4 = connection.prepareStatement(q4);
-                st4.setString(1, profile.getSurname());
-                st4.setString(2, profile.getMail());
-
-                st4.executeUpdate();
-            }
-
-            if(profile.getBiography()!=oldProfile.getBiography() && profile.getBiography()!=null) { //update biography
-
-                String q5 = "UPDATE Profile SET biography=? WHERE mail=?;";
-
-                PreparedStatement st5 = connection.prepareStatement(q5);
-                st5.setString(1, profile.getBiography());
-                st5.setString(2, profile.getMail());
-
-                st5.executeUpdate();
-            }
-
-            if(profile.getProfilePicture()!=oldProfile.getProfilePicture() && profile.getProfilePicture()!=null) {  //update profile picture
-
-                String q6 = "UPDATE Profile SET profilePicture=? WHERE mail=?;";
-
-                PreparedStatement st6 = connection.prepareStatement(q6);
-                st6.setBlob(1, profile.getProfilePicture());
-                st6.setString(2, profile.getMail());
-
-                st6.executeUpdate();
+            } else {
+                //THROW EXCEPION
             }
 
         } catch (Exception e){
@@ -157,13 +145,14 @@ public class ProfileDAO implements IProfileDAO {
         DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
     }
 
-
     @Override
     public JBProfile get(JBProfile profile) {            //retrieve profile from database
         return getProfileByMail(profile.getMail());
     }
 
 
+
+    //PROTECTED METHODS:
     protected JBProfile getProfileByMail(String mail) {
 
         JBProfile profileOut = getUserByMail(mail);                 //check if profile is in User table
@@ -171,7 +160,6 @@ public class ProfileDAO implements IProfileDAO {
 
         return profileOut;
     }
-
 
     protected Artist getArtistByMail(String mail) {
         connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
@@ -206,7 +194,6 @@ public class ProfileDAO implements IProfileDAO {
 
         return result;
     }
-
 
     protected User getUserByMail(String mail) {
         connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
@@ -244,6 +231,8 @@ public class ProfileDAO implements IProfileDAO {
     }
 
 
+
+    //PRIVATE METHODS:
     private Time getMinuteListenedByUserMail(String userMail) {
         connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
         PreparedStatement st;
@@ -270,6 +259,158 @@ public class ProfileDAO implements IProfileDAO {
 
         return result;
 
+    }
+
+    private void updateUsername(JBProfile profile) {
+        connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
+
+        try {
+            String q1 = "UPDATE Profile SET username=? WHERE mail=?;";
+
+            PreparedStatement st1 = connection.prepareStatement(q1);
+            st1.setString(1, profile.getUsername());
+            st1.setString(2, profile.getMail());
+
+            st1.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
+    }
+
+    private void updatePassword(JBProfile profile) {
+        connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
+
+        try {
+            String q2 = "UPDATE Profile SET password=? WHERE mail=?;";
+
+            PreparedStatement st2 = connection.prepareStatement(q2);
+            st2.setString(1, profile.getPassword());
+            st2.setString(2, profile.getMail());
+
+            st2.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
+    }
+
+    private void updateName(JBProfile profile) {
+        connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
+
+        try {
+            String q3 = "UPDATE Profile SET name=? WHERE mail=?;";
+
+            PreparedStatement st3 = connection.prepareStatement(q3);
+            st3.setString(1, profile.getName());
+            st3.setString(2, profile.getMail());
+
+            st3.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
+    }
+
+    private void updateSurname(JBProfile profile) {
+        connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
+
+        try {
+            String q4 = "UPDATE Profile SET surname=? WHERE mail=?;";
+
+            PreparedStatement st4 = connection.prepareStatement(q4);
+            st4.setString(1, profile.getSurname());
+            st4.setString(2, profile.getMail());
+
+            st4.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
+    }
+
+    private void updateBiography(JBProfile profile) {
+        connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
+
+        try {
+            String q5 = "UPDATE Profile SET biography=? WHERE mail=?;";
+
+            PreparedStatement st5 = connection.prepareStatement(q5);
+            st5.setString(1, profile.getBiography());
+            st5.setString(2, profile.getMail());
+
+            st5.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
+    }
+
+    private void updateProfilePicture(JBProfile profile) {
+        connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
+
+        try {
+            String q6 = "UPDATE Profile SET profilePicture=? WHERE mail=?;";
+
+            PreparedStatement st6 = connection.prepareStatement(q6);
+            st6.setBlob(1, profile.getProfilePicture());
+            st6.setString(2, profile.getMail());
+
+            st6.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
+    }
+
+    private void updateVisibility(User user) {
+        connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
+
+        try {
+            String q7 = "UPDATE User SET isVisible=? WHERE mail=?;";
+
+            PreparedStatement st7 = connection.prepareStatement(q7);
+            st7.setBoolean(1, user.isVisible());
+            st7.setString(2, user.getMail());
+
+            st7.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
+    }
+
+    private void updateTotalListeners(Artist artist) {
+        connection = DBManagerFactory.getInstance().getDBManager().startConnection(connection, schema);
+
+        try {
+            String q8 = "UPDATE Artist SET totalListeners=? WHERE mail=?;";
+
+            PreparedStatement st8 = connection.prepareStatement(q8);
+            st8.setInt(1, artist.getTotalListeners());
+            st8.setString(2, artist.getMail());
+
+            st8.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        DBManagerFactory.getInstance().getDBManager().closeConnection(connection);
     }
 
 }
